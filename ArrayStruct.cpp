e64a571b5e2e917e7c000dd9c80538f52d07176f
nauflabhy <n4uf4l.ku@gmail.com>
2025-04-15 21:15:29 +0800

    ArrayStruct4

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -11,9 +11,16 @@ struct mahasiswa {
     string nama;
     string alamat;
 };
-int main(){
+int main() {
     for (int m = 0; m < 2; m++)
+    mahasiswa mhs[2];
 
     {
-      
+        cout << "Data ke-" << m + 1 << endl;
+        cout << "NIM  :"; cin >> mhs[m].nim;
+        cout << "NAMA :"; cin.ignore(); getline(cin, mhs[m].nama);
+        cout << "ALAMAT KOTA  :"; getline(cin,mhs[m].alamat.kota);
+        cout << endl;
     }
+
+    

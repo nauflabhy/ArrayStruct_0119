8a1970dea5a9f71adb86b0b0165237d4d266f4e7
nauflabhy <n4uf4l.ku@gmail.com>
2025-04-20 16:38:03 +0800

    array5

--- struct.cpp
+++ struct.cpp
@@ -23,3 +23,8 @@ int main()
     cout << endl;
     cout << "Menampilkan Data" << endl;
     cout << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}
